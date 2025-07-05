d338508d3c3c4b799405d112577e3d3a9256f4c9
Ramakant Sanga <rssanga@gmail.com>
2025-07-05 17:48:31 +0530

    laptop last commit

---
+++ Dictionary and Set.py
@@ -0,0 +1,12 @@
+student = {
+    "name" : "RAMAKANT",
+    "Subject" : {
+        "Maths" : 90,
+        "English" : 70,
+    }
+}
+
+print("Student Name" , student["name"])
+print("Maths Marks:", student["Subject"]["Maths"])
+print("English Marks:", student["Subject"]["English"])
+
---
+++ List_&_Tuple.py
@@ -0,0 +1,32 @@
+# WAP to ask user to enter name of their favorite movies name and store them in a list
+
+# movies = []
+# movies.append(input("Enter your 1st Movie:"))
+# movies.append(input("Enter your 2nd Movie:"))
+# movies.append(input("Enter your 3rd Movie:"))
+
+# print(movies)
+
+#WAP to check if a list contains a palindrome of elements (Hint: use copy() methodes)
+
+# list1 = [1,2,1]
+# list2 = [1,2,3]
+# copy_list1 = list1.copy()
+# copy_list1.reverse
+
+# if (copy_list1 == list1):
+#     print("Its Palindrome value")
+# else:
+#     print("this is not palindrome value")
+
+
+# # WAP to count the numbers of students with the "A" grade in the foillowing Tuple
+
+# grade = ("B","C","A","A","D","A","E")
+# print (grade.count("A"))
+
+# #Store the above values in the list and sort them from "A" to "E"
+
+# grade = ["B","C","A","A","D","A","E"]
+# grade.sort()
+# print(grade)
